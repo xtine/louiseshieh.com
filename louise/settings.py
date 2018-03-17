123b391e3e08dc5f031ffe11f3df580b35372c61
xtine <christine@fieryorange.com>
2018-03-16 21:47:19 -0600

    setup whitenoise

--- louise/settings.py
+++ louise/settings.py
@@ -24,6 +24,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
