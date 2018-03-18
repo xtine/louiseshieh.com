27b2f96e41fe9ce70af547bf804d785bcc5abe78
xtine <christine@fieryorange.com>
2018-03-17 21:05:36 -0600

    set up about page

--- louise/urls.py
+++ louise/urls.py
@@ -8,6 +8,7 @@ from portfolio import views
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^about/', views.about, name='about'),
     url(r'^$', views.home, name='home'),
 ]
 
--- portfolio/views.py
+++ portfolio/views.py
@@ -8,7 +8,18 @@ from .models import Project, AboutPage
 
 def home(request):
     projects = Project.objects.order_by('home_page_order')
+    page_name = 'home'
 
     return render(request, 'index.html', {
-        'projects': projects
+        'projects': projects,
+        'page': page_name
+    })
+
+def about(request):
+    about = AboutPage.objects.first()
+    page_name = 'about'
+
+    return render(request, 'about.html', {
+        'about': about,
+        'page': page_name
     })
